38549d37ceb8aa233c4b7e7f674f43e88aa633d7
Logmytech <Logmytech@users.noreply.github.com>
2014-12-27 16:37:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import NumericProperty
 from kivy.lang import Builder
 from kivy.network.urlrequest import UrlRequest
+from bs4 import BeautifulSoup
 import urllib
-import re
-
-
 
 
 
@@ -16,68 +14,40 @@ class RootWidget(ScreenManager):
        you can use any other layout/widget depending on your usage.
     '''
     def post_last(self, req, result):
-
-    	self.roosvelt.text = "Loading............"
         try:
-        	self.roosvelt.text ="Owner Name: "+ re.findall(r'"ctl00_OnlineContent_tdOwner" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*\s*[^<])<', result)[0]+"\n"*2
-        except:
-        	self.roosvelt.text = "404 error, Number not found"
-        try:
-        	self.roosvelt.text += "Colour: "+ re.findall(r'"ctl00_OnlineContent_tdColor" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass	
-        try:	
-        	self.roosvelt.text += "Vehicle type: "+ re.findall(r'"ctl00_OnlineContent_tdVehClass" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass	
-        try:	
-        	self.roosvelt.text += "Maker Name: "+ re.findall(r'"ctl00_OnlineContent_tdMkrName" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass	
-        try:	
-        	self.roosvelt.text += "Fuel type: "+ re.findall(r'"ctl00_OnlineContent_tdFuelType" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass	
-        try:	
-        	self.roosvelt.text += "Registered date: "+ re.findall(r'"ctl00_OnlineContent_tdDOR" align="left" colspan="2">([^>]\d*/*\d*/*\d*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass
-        try:	
-        	self.roosvelt.text += "Engine number: "+ re.findall(r'"ctl00_OnlineContent_tdEngNo" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass	
-        try:	
-        	self.roosvelt.text += "Chassis No: "+ re.findall(r'"ctl00_OnlineContent_tdChassisno" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass	
-        try:
-        	self.roosvelt.text += "Status: "+ re.findall(r'"ctl00_OnlineContent_tdStatus" align="left" colspan="2">([^>]\w*\s*\w*\s*\w*[^<])<', result)[0]+"\n"*2
-        except:
-        	pass
-
+            soup = BeautifulSoup(result)
+            var ="Owner: "+ soup.find(id="ctl00_OnlineContent_tdOwner").text+"\n"*2
+            var+= "Vehicle Class: "+ soup.find(id="ctl00_OnlineContent_tdVehClass").text+"\n"*2
+            var+= "Colour: "+ soup.find(id="ctl00_OnlineContent_tdColor").text+"\n"*2
+            var+= "Maker's Name: "+ soup.find(id="ctl00_OnlineContent_tdMkrName").text+"\n"*2
+            var+= "Mfg.Year: "+ soup.find(id="ctl00_OnlineContent_tdMfgYear").text+"\n"*2
+            var+= "Maker's Class: " + soup.find(id="ctl00_OnlineContent_tdMkrClass").text+"\n"*2
+            var+= "Engine No.: " + soup.find(id="ctl00_OnlineContent_tdEngNo").text+"\n"*2
+            var+= "Registration Date: " + soup.find(id="ctl00_OnlineContent_tdDOR").text+"\n"*2
+            var+= "Chassis No: " + soup.find(id="ctl00_OnlineContent_tdChassisno").text+"\n"*2
+            self.roosvelt.text= var    
+        except Exception, e:
+            self.roosvelt.text= "Invalid input, Try again!"
 
     def post_again(self, t):
-    	self.roosvelt.text = "Loading......."
         headers = {'Content-type': 'application/x-www-form-urlencoded','Accept': 'text/plain'}
         params = urllib.urlencode({'__VIEWSTATE': t, 'ctl00$OnlineContent$btnGetData':"Get Data","ctl00$OnlineContent$txtInput":self.search_input.text,"ct0":"R"})
-        req = UrlRequest('https://aptransport.in/APCFSTONLINE/Reports/VehicleRegistrationSearch.aspx', on_success=self.post_last, req_headers=headers,req_body=params,on_error=self.error,on_redirect=self.error ,on_failure=self.error)
-        
+        req = UrlRequest('https://aptransport.in/APCFSTONLINE/Reports/VehicleRegistrationSearch.aspx', on_success=self.post_last, req_headers=headers,req_body=params)
 
     def posted(self, req, result):
-        self.roosvelt.text = "Loading..."
-        print result
-        t= re.findall(r'"__VIEWSTATE" value="(/.+)"', result)
-        print t
-        self.post_again(t[0])
-    
-    def error(self,req, result):
-    	self.roosvelt.text = str(result)
-        
+        soup = BeautifulSoup(result)
+        print soup
+        m= soup.find(id= "__VIEWSTATE")
+        t= m.get('value')
+        self.post_again(t)
+
     def postbug(self):
-        self.roosvelt.text = "Loading"
+		    
         headers = {'Content-type': 'application/x-www-form-urlencoded','Accept': 'text/plain'}
-        req = UrlRequest('https://aptransport.in/APCFSTONLINE/Reports/VehicleRegistrationSearch.aspx', on_success=self.posted,on_error=self.error,on_redirect=self.error ,on_failure=self.error ,req_headers=headers)
-        
+        req = UrlRequest('https://aptransport.in/APCFSTONLINE/Reports/VehicleRegistrationSearch.aspx', on_success=self.posted, req_headers=headers)
+    
+    pass
+
 class MainApp(App):
     '''This is the main class of your app.
        Define any app wide entities here.
